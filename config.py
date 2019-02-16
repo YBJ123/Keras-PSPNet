3fe73bcfc50dffc716652ab0a493a8463a11753b
liminn <11172918@qq.com>
2019-02-16 17:16:07 +0800

    modify

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ unknown_code = 128
 num_classes = 3
 
 # 训练集占总数据集的80%
-num_train_samples = 11754 
+num_train_samples = 8835
 # 验证集占总数据集的20%         
-num_valid_samples = 2939           
+num_valid_samples = 2209          
 batch_size = 4
 epochs = 100
 patience = 10
@@ -19,7 +19,7 @@ inference_output_path_base = "inference_output"
 
 # train image path
 #rgb_image_path = '/home/datalab/ex_disk1/bulang/segmentation_data_repository/原始数据/T-Net-Person/image'
-rgb_image_path = "../SemanticHumanMatting/data_shm_person/Training_set/composite"
+rgb_image_path = "../SemanticHumanMatting/data_shm_id_photo/Training_set/composite"
 # train mask path
 #mask_img_path = '/home/datalab/ex_disk1/bulang/segmentation_data_repository/原始数据/T-Net-Person/mask'
-mask_img_path = "../SemanticHumanMatting/data_shm_person/Training_set/alpha"
+mask_img_path = "../SemanticHumanMatting/data_shm_id_photo/Training_set/alpha"
